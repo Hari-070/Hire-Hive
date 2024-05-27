f04458c94e9be1b7dd7db04f93bf47e6d8a03215
RishabTR <rishab.official.8@gmail.com>
2024-05-27 19:08:09 +0530

    Use this template

---
+++ src/screens/WelcomeScreen.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const WelcomeScreen = () => {
+  return (
+    <Text>WelcomeScreen</Text>
+  )
+}
+
+export default WelcomeScreen
+
+const styles = StyleSheet.create({})
