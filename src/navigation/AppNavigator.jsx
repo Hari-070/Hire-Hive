e71bc245fd4e45f71a8149b50aa0758a95716df3
RishabTR <rishab.official.8@gmail.com>
2024-06-03 11:02:48 +0530

    recent work regarding screens

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -1,21 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import WelcomeScreen from "../screens/WelcomeScreen";
 const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
 
-const AppNavigator = () => {
+import SignIn from "../screens/SignIn";
+import HomeScreen from "../screens/HomeScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
+ 
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="welcome" component={WelcomeScreen}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
-
-export default AppNavigator;
-
-const styles = StyleSheet.create({});
+export default App;
